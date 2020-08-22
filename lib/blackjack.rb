74fc62659e54c58c01ca15d3716ceb29ecfd727f
James P Bondeson <jpbond58@gmail.com>
2020-08-22 18:35:19 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,13 +30,13 @@ def initial_round
   return card_sum
 end
 
-def hit?
+def hit?(card_total)
   prompt_user
   user_input = get_user_input
   if user_input == "h"
     card_total += deal_card
   elsif user_input == "s"
-    card_total 
+    card_total
     invalid_command
   end
 end
